fe897846beacdcb43520d587ef622e6e09396005
RizanwimTakwaRosakyar <145947227+RizanwimTakwaRosakyar@users.noreply.github.com>
2025-04-14 10:46:12 +0800

    Add files via upload

---
+++ pre-pratikum/pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Rizanwim
+-- NIM : 23241029
+-- KELAS : A
+
+-- melihat Database yang digunakan
+USE mysql;
+
+-- mendeskripsikan tabel user 
+DESCRIBE user;
+
+-- menampilkan tabel dalam data base mysql
+SHOW TABLES;
+
+-- mendeskripsikan tabel user
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom Host,User,Password
+SELECT Host , User , Pasword FROM user;
